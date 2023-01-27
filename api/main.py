e3179bef8e12698e0c0c2ae521fb97d070a22e11
Gauthier Jolly <gauthier.jolly@canonical.com>
2023-01-27 13:24:48 +0100

    main endpoint implemented

--- api/main.py
+++ api/main.py
@@ -1,8 +1,200 @@
 import requests
+import click
+import os
+import urllib
+import json
+
+API_BASE_URL = 'https://penguinatorapi2.azure-api.net/'
+
+TENANT_ID = os.getenv("AZURE_TENANT_ID")
+SUBSCRIPTION_ID = os.getenv("AZURE_SUBSCRIPTION_ID")
+CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+
+def auth() -> str:
+    auth_url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
+    scope = "api://7395f717-3e16-4e02-b95f-802a15a82d47/.default"
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "scope": scope,
+    }
+    resp = requests.post(auth_url, data=data)
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    resp_json = resp.json()
+    token = resp_json['access_token']
+    return token
 
 
-def main():
-    pass
+@click.group()
+@click.pass_context
+def cli(ctx):
+    token = auth()
+    ctx.obj = dict()
+    session = requests.session()
+    session.headers.update({'Authorization': f'Bearer {token}'})
+    ctx.obj['session'] = session
+
+
+@cli.command()
+@click.pass_context
+def list_projects(ctx):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "projects")
+
+    session = ctx.obj['session']
+
+    resp = session.get(endpoint)
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+@click.option('--project-id', '-p', required=True)
+def get_projects(ctx, project_id: str):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "projects")
+
+    session = ctx.obj['session']
+
+    resp = session.get(
+        endpoint,
+        params={'id': project_id}
+    )
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+def list_jobs(ctx):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
+
+    session = ctx.obj['session']
+
+    resp = session.get(endpoint)
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+@click.option('--job-id', '-j', required=True)
+def get_job(ctx, job_id: str):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
+
+    session = ctx.obj['session']
+
+    resp = session.get(
+        endpoint,
+        params={'id': job_id}
+    )
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+def list_subscriptions(ctx):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "subscriptions")
+
+    session = ctx.obj['session']
+
+    resp = session.get(
+        endpoint,
+    )
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+def list_test_suites(ctx):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "suites")
+
+    session = ctx.obj['session']
+
+    resp = session.get(
+        endpoint,
+    )
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
+
+
+@cli.command()
+@click.pass_context
+@click.option('--project', '-p', required=True)
+@click.option('--test-suite', '-t', required=True)
+@click.option('--subscription', '-s', required=True)
+@click.option('--marketplace-image-urn', '-u', type=str)
+@click.option('--vhd-sas-url', '-v', type=str)
+@click.option('--vm-generation', '-g', default=2, type=int)
+def create_job(ctx, project: str, test_suite: str, subscription: str, marketplace_image_urn: str, vhd_sas_url: str, vm_generation: int):
+    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
+
+    session = ctx.obj['session']
+    payload = {
+        'test_project': project,
+        'test_suite': test_suite,
+        'subscription': subscription,
+        'vm_generation': vm_generation,
+    }
+
+    if marketplace_image_urn:
+        payload['marketplace_image_urn'] = marketplace_image_urn
+    elif vhd_sas_url:
+        payload['vhd_sas_url'] = vhd_sas_url
+    else:
+        raise ValueError(
+            "One of --vhd-sas-url or --marketplace-image-urn should be passed."
+        )
+
+    resp = session.post(
+        endpoint,
+        json=payload
+    )
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json()))
 
 
 if __name__ == "___main__":
