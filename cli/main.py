58ca2c99a23e8bbfa11dc14eed2d5d66204787e5
Chi Song <chisong@microsoft.com>
2023-05-23 14:50:05 -0700

    Migrated to new API

--- cli/main.py
+++ cli/main.py
@@ -1,25 +1,104 @@
 import requests
 import click
 import os
-import urllib
 import json
 
-API_BASE_URL = 'https://penguinatorapi2.azure-api.net/'
+
+RESOURCE_PROVIDER = "Microsoft.AzureImageTestingForLinux"
+API_VERSION = "2023-08-01-preview"
 
 TENANT_ID = os.getenv("AZURE_TENANT_ID")
 SUBSCRIPTION_ID = os.getenv("AZURE_SUBSCRIPTION_ID")
 CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+RESOURCE_GROUP = os.getenv("AZURE_RESOURCE_GROUP")
+
+
+def _get_endpoint(segment, **kwargs):
+    resource_group = kwargs.get("resource_group")
+    return (
+        f"https://eastus2euap.management.azure.com/subscriptions/{SUBSCRIPTION_ID}/"
+        f"resourceGroups/{resource_group}/providers/{RESOURCE_PROVIDER}/"
+        f"{segment}?api-version={API_VERSION}"
+    )
+
+
+def _common_options(func):
+    options = [
+        click.option(
+            "--resource-group",
+            "-rg",
+            default=RESOURCE_GROUP,
+            help="Azure Resource Group name",
+        ),
+    ]
+    return _apply_options(func, options)
+
+
+def _template_options(func):
+    options = [
+        click.option("--vm-size", "-s", help="VM size"),
+        click.option("--priority", "-p", help="Test case priority"),
+        click.option("--case-name", "-c", help="Test case names"),
+        click.option(
+            "--location",
+            "-l",
+            default="westus3",
+            help="job template location, it's not required to be changed",
+        ),
+    ]
+    return _apply_options(func, options)
+
+
+def _apply_options(func, options):
+    for option in reversed(options):
+        func = option(func)
+    return func
+
+
+def _create_template(vm_size, priority, case_name, location):
+    template = {
+        "templateTags": [],
+    }
+
+    selection = {}
+    if priority:
+        selection["casePriority"] = [int(x) for x in priority.split(",") if x]
+    if case_name:
+        selection["caseName"] = [x for x in case_name.split(",") if x]
+
+    if selection:
+        template["selections"] = [selection]
+    if location:
+        template["region"] = [location]
+    else:
+        template["region"] = []
+    if vm_size:
+        template["vmSize"] = [vm_size]
+    else:
+        template["vmSize"] = []
+
+    return template
+
+
+def _output_result(resp):
+    try:
+        resp.raise_for_status()
+    except:
+        print(resp.json())
+        raise
+
+    print(json.dumps(resp.json(), indent=2))
 
 
 def auth() -> str:
-    auth_url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
-    scope = "api://7395f717-3e16-4e02-b95f-802a15a82d47/.default"
+    auth_url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/token"
+    resource = "https://management.azure.com/"
     data = {
         "grant_type": "client_credentials",
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
-        "scope": scope,
+        "resource": resource,
     }
     resp = requests.post(auth_url, data=data)
     try:
@@ -29,7 +108,7 @@ def auth() -> str:
         raise
 
     resp_json = resp.json()
-    token = resp_json['access_token']
+    token = resp_json["access_token"]
     return token
 
 
@@ -37,193 +116,170 @@ def auth() -> str:
 @click.pass_context
 def cli(ctx):
     """
-    CLI client for the Penguinator API (https://penguinatorstarter.powerappsportals.com/webapis2/)
+    CLI client for the Azure Image Testing for Linux API
     """
     token = auth()
     ctx.obj = dict()
     session = requests.session()
-    session.headers.update({'Authorization': f'Bearer {token}'})
-    ctx.obj['session'] = session
+    session.headers.update({"Authorization": f"Bearer {token}"})
+    ctx.obj["session"] = session
 
 
 @cli.command()
 @click.pass_context
-def list_projects(ctx):
+@_common_options
+def list_jobtemplates(ctx, **kwargs):
     """
-    List all the test projects
+    List all the test job templates.
     """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "projects")
+    endpoint = _get_endpoint("jobTemplates", **kwargs)
 
-    session = ctx.obj['session']
+    session = ctx.obj["session"]
 
     resp = session.get(endpoint)
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
+    _output_result(resp)
 
 
 @cli.command()
 @click.pass_context
-@click.option('--project-id', '-p', required=True)
-def get_projects(ctx, project_id: str):
+@_common_options
+@click.option("--name", "-n", help="job template name", required=True)
+def get_jobtemplate(ctx, name: str, **kwargs):
     """
-    Get a test project
+    Get a test job template.
     """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "projects")
+    endpoint = _get_endpoint(f"jobTemplates/{name}", **kwargs)
 
-    session = ctx.obj['session']
-
-    resp = session.get(
-        endpoint,
-        params={'id': project_id}
-    )
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
-
-
-@cli.command()
-@click.pass_context
-def list_jobs(ctx):
-    """
-    List all the test jobs
-    """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
-
-    session = ctx.obj['session']
+    session = ctx.obj["session"]
 
     resp = session.get(endpoint)
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
+    _output_result(resp)
 
 
 @cli.command()
 @click.pass_context
-@click.option('--job-id', '-j', required=True)
-def get_job(ctx, job_id: str):
+@_common_options
+@click.option("--name", "-n", help="job template name", required=True)
+@_template_options
+def create_jobtemplate(
+    ctx, name: str, vm_size: str, priority: str, case_name: str, location: str, **kwargs
+):
+    """
+    Create a new test job template
+    """
+    endpoint = _get_endpoint(f"jobTemplates/{name}", **kwargs)
+
+    session = ctx.obj["session"]
+
+    template = _create_template(vm_size, priority, case_name, location)
+
+    payload = {"location": location, "name": name, "properties": template}
+    resp = session.put(endpoint, json=payload)
+    _output_result(resp)
+
+
+@cli.command()
+@click.pass_context
+@_common_options
+def list_jobs(ctx, **kwargs):
+    """
+    List all the test jobs.
+    """
+    endpoint = _get_endpoint("jobs", **kwargs)
+
+    session = ctx.obj["session"]
+
+    resp = session.get(endpoint)
+    _output_result(resp)
+
+
+@cli.command()
+@click.pass_context
+@_common_options
+@click.option("--name", "-n", required=True)
+def get_job(ctx, name: str, **kwargs):
     """
     Get a test job. Can be used to get the current status of the job.
     """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
+    endpoint = _get_endpoint(f"jobs/{name}", **kwargs)
 
-    session = ctx.obj['session']
+    session = ctx.obj["session"]
 
-    resp = session.get(
-        endpoint,
-        params={'id': job_id}
-    )
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
+    resp = session.get(endpoint)
+    _output_result(resp)
 
 
 @cli.command()
 @click.pass_context
-def list_subscriptions(ctx):
-    """
-    List the subscriptions that can be used for testing
-    """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "subscriptions")
-
-    session = ctx.obj['session']
-
-    resp = session.get(
-        endpoint,
-    )
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
-
-
-@cli.command()
-@click.pass_context
-def list_test_suites(ctx):
-    """
-    List the test suite available
-    """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "suites")
-
-    session = ctx.obj['session']
-
-    resp = session.get(
-        endpoint,
-    )
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
-
-    print(json.dumps(resp.json()))
-
-
-@cli.command()
-@click.pass_context
-@click.option('--project', '-p', required=True, help='ID of the test project to use')
-@click.option('--test-suite', '-t', required=True, help='Test suite to use')
-@click.option('--subscription', '-s', required=True, help='Internal penguinator ID of the "subscription" to use')
-@click.option('--marketplace-image-urn', '-u', type=str, help='URN of the image ("az vm image list -p Canonical --all")')
-@click.option('--vhd-sas-url', '-v', type=str, help='SAS URL of a VHD to test')
-@click.option('--vm-generation', '-g', default=2, type=int, help="Hyper-V generation (1 or 2)")
-@click.option('--vm-size', '-m', help="VM size")
-def create_job(ctx, project: str, test_suite: str, subscription: str, marketplace_image_urn: str, vhd_sas_url: str, vm_generation: int, vm_size: str):
+@_common_options
+@click.option("--name", "-n", required=True, help="job name")
+@click.option(
+    "--marketplace-image-urn",
+    "-u",
+    help='URN of the image ("az vm image list -p Canonical --all")',
+)
+@click.option("--vhd-sas-url", "-v", type=str, help="SAS URL of a VHD to test")
+@click.option(
+    "--vm-generation", "-g", default=2, type=int, help="Hyper-V generation (1 or 2)"
+)
+@click.option("--template-name", "-t", type=str, help="job template name")
+@_template_options
+def create_job(
+    ctx,
+    name: str,
+    marketplace_image_urn,
+    vhd_sas_url: str,
+    template_name: str,
+    vm_generation: int,
+    vm_size: str,
+    priority: str,
+    case_name: str,
+    location: str,
+    **kwargs,
+):
     """
     Create a new test job
     """
-    endpoint = urllib.parse.urljoin(API_BASE_URL, "jobs")
+    endpoint = _get_endpoint(f"jobs/{name}", **kwargs)
 
-    session = ctx.obj['session']
-    payload = {
-        'test_project': project,
-        'test_suite': test_suite,
-        'azure_subscription': subscription,
-        'vm_generation': vm_generation,
+    session = ctx.obj["session"]
+
+    image = {
+        "vhdGeneration": vm_generation,
+        "architecture": "x64",
     }
-
     if marketplace_image_urn:
-        payload['marketplace_image_urn'] = marketplace_image_urn
+        image["type"] = "marketplace"
+        image_parts = marketplace_image_urn.split(":")
+        if len(image_parts) != 4:
+            raise ValueError(
+                "marketplace_image_urn should be in the format of 'publisher:offer:sku:version'"
+            )
+        image["publisher"] = image_parts[0]
+        image["offer"] = image_parts[1]
+        image["sku"] = image_parts[2]
+        image["version"] = image_parts[3]
     elif vhd_sas_url:
-        payload['vhd_sas_url'] = vhd_sas_url
+        image["type"] = "vhd"
+        image["url"] = vhd_sas_url
     else:
         raise ValueError(
             "One of --vhd-sas-url or --marketplace-image-urn should be passed."
         )
 
-    if vm_size:
-        payload['vm_size'] = vm_size
+    template = _create_template(vm_size, priority, case_name, location)
 
-    resp = session.post(
-        endpoint,
-        json=payload
-    )
-    try:
-        resp.raise_for_status()
-    except:
-        print(resp.json())
-        raise
+    payload = {
+        "location": location,
+        "properties": {
+            "jobTemplateName": template_name,
+            "jobTemplateInstance": template,
+            "image": image,
+        },
+    }
 
-    print(json.dumps(resp.json()))
+    resp = session.put(endpoint, json=payload)
+    _output_result(resp)
 
 
 if __name__ == "___main__":
